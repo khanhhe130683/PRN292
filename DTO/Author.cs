adea1c2c713b44be1a7e38db72b580922b935311
Trương Quốc Khánh <59003477+khanhhe130683@users.noreply.github.com>
2020-07-15 18:02:16 +0700

    Add files via upload

---
+++ DTO/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Author
+    {
+        public string AuthorID { get; set; }
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+
+        public Author()
+        {
+
+        }
+
+        public Author(string id, string displayname, string username)
+        {
+            this.AuthorID = id;
+            this.DisplayName = displayname;
+            this.UserName = username;
+        }
+    }
+}
