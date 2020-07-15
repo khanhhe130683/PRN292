3e39eff42177fa599ddee0d19632eb2007508a30
Trương Quốc Khánh <59003477+khanhhe130683@users.noreply.github.com>
2020-07-15 18:03:27 +0700

    Add files via upload

---
+++ DTO/News.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class News
+    {
+        public string NewId { get; set; }
+        public string AuthorID { get; set; }
+        public string CategoryID { get; set; }
+        public string PublishDate { get; set; }
+        public string Content { get; set; }
+        public string CoverImage { get; set; }
+
+        public News()
+        {
+
+        }
+        public News(string newID, string authorID, string categoryID, string publishdate, string content, string coverimage)
+        {
+            this.NewId = newID;
+            this.AuthorID = authorID;
+            this.CategoryID = categoryID;
+            this.PublishDate = publishdate;
+            this.Content = content;
+            this.CoverImage = coverimage;
+        }
+    }
+}
