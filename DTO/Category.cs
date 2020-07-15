945410056c5b013173e13cea8360d5496452a718
Trương Quốc Khánh <59003477+khanhhe130683@users.noreply.github.com>
2020-07-15 18:02:34 +0700

    Add files via upload

---
+++ DTO/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Category
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryUrl { get; set; }
+
+        public Category()
+        {
+
+        }
+        public Category(string categoryID, string categoryName, string url)
+        {
+            this.CategoryID = categoryID;
+            this.CategoryName = categoryName;
+            this.CategoryUrl = url;
+        }
+    }
+}
