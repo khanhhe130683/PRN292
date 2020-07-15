17f96b61de470304fb0fbb556edaf4391ab19d0e
Trương Quốc Khánh <59003477+khanhhe130683@users.noreply.github.com>
2020-07-15 18:01:47 +0700

    Add files via upload

---
+++ DTO/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Joindate { get; set; }
+        public string Role { get; set; }
+
+        public Account() { }
+        public Account(string username, string password, string fullname, string email, string joindate, string role)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Fullname = fullname;
+            this.Email = email;
+            this.Joindate = joindate;
+            this.Role = role;
+        }
+    }
+}
